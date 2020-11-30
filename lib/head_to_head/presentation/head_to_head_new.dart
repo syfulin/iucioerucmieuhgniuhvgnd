f9d6fc4ec4bc36129eddff845fe98f90afaa4803
Maxim Dymov <m.dymov@meteora.pro>
2020-11-30 21:04:26 +1000

    Update head to head

--- lib/head_to_head/presentation/head_to_head_new.dart
+++ lib/head_to_head/presentation/head_to_head_new.dart
@@ -1,116 +1,131 @@
 import 'package:flutter/material.dart';
 
 class HeadToHeadNew extends StatelessWidget {
-  final String _matchId;
-
-  const HeadToHeadNew({
-    @required String matchId,
-  }) : _matchId = matchId;
+  const HeadToHeadNew();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        // appBar: AppBar(
-        //   title: const Text('Статистика'),
-        // ),
-        // Основная область экрана
-        body: Column(
-          children: [
-            // 1. Виджет Live-статистики
-            Expanded(
-              flex: 30, // Занимает 30 частей экрана (30 %)
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // Левый блок (первая команда)
-                  Expanded(
-                    flex: 60,
-                    child: Container(
-                      color: Color.fromARGB(204, 196, 196, 196),
-                      width: 60,
-                      height: double.infinity,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.ac_unit,
-                            size: 45,
-                          ),
-                          SizedBox(height: 6),
-                          Text('УФА', style: TextStyle(fontSize: 17)),
-                        ],
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text('Статистика'),
+      // ),
+      // Основная область экрана
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Column(
+            children: [
+              // 1. Виджет Live-статистики
+              Expanded(
+                flex: 30, // Занимает 30 частей экрана (30 %)
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Левый блок (первая команда)
+                    Expanded(
+                      flex: 60,
+                      child: Container(
+                        color: Color.fromARGB(204, 196, 196, 196),
+                        width: 60,
+                        height: double.infinity,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.ac_unit,
+                              size: 45,
+                            ),
+                            SizedBox(height: 6),
+                            Text('УФА', style: TextStyle(fontSize: 17)),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  // Статистика
-                  Expanded(
-                    flex: 231,
-                    child: Container(
-                      color: Colors.red,
-                      // constraints:
-                      //     BoxConstraints(minHeight: 231, maxHeight: 231),
-                      // height: 350,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            children: [
-                              Text('МАТЧ СОСТОЯЛСЯ',
-                                  style: TextStyle(fontSize: 21)),
-                              Text('22.11.2020',
-                                  style: TextStyle(fontSize: 21)),
-                            ],
-                          ),
-                          Text('9:9', style: TextStyle(fontSize: 60)),
-                          Container(
-                            width: 251,
-                            // color: Colors.red,
-                            child: Statistics(),
-                          ),
-                        ],
+                    // Статистика
+                    Expanded(
+                      flex: 231,
+                      child: Container(
+                        color: Colors.red,
                       ),
                     ),
-                  ),
-                  // Правый блок (вторая команда)
-                  Expanded(
-                    flex: 60,
-                    child: Container(
-                      color: Color.fromARGB(204, 196, 196, 196),
-                      width: 60,
-                      height: double.infinity,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.ac_unit,
-                            size: 45,
-                          ),
-                          SizedBox(height: 6),
-                          Text('УФА', style: TextStyle(fontSize: 17)),
-                        ],
+                    // Правый блок (вторая команда)
+                    Expanded(
+                      flex: 60,
+                      child: Container(
+                        color: Color.fromARGB(204, 196, 196, 196),
+                        width: 60,
+                        height: double.infinity,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.ac_unit,
+                              size: 45,
+                            ),
+                            SizedBox(height: 6),
+                            Text('УФА', style: TextStyle(fontSize: 17)),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            // Вся остальная часть экрана (где делать ставки и т д)
-            Expanded(
-              flex: 70, // Занимает 70 частей экрана (70 %)
-              child: Container(
-                color: Colors.blue,
+              // Вся остальная часть экрана (где делать ставки и т д)
+              Expanded(
+                flex: 70, // Занимает 70 частей экрана (70 %)
+                child: Container(
+                  color: Colors.blue,
+                ),
               ),
-            ),
-          ],
-        ),
+            ],
+          ),
 
-        // 2. Тестовый виджет (отдельный виджет-список статистики)
-        // body: Container(
-        //   width: 251,
-        //   color: Colors.red,
-        //   child: Statistics(),
-        // ),
+          // 2. Тестовый виджет (отдельный виджет-список статистики)
+          // body: Container(
+          //   width: 251,
+          //   color: Colors.red,
+          //   child: Statistics(),
+          // ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                flex: 60,
+                child: Container(
+                  color: Colors.transparent,
+                ),
+              ),
+              Expanded(
+                flex: 231,
+                child: Container(
+                  // color: Colors.white,
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      Column(
+                        children: [
+                          Text('МАТЧ СОСТОЯЛСЯ',
+                              style: TextStyle(fontSize: 15)),
+                          Text('22.11.2020', style: TextStyle(fontSize: 15)),
+                        ],
+                      ),
+                      
+                      Text('9:9', style: TextStyle(fontSize: 37.5)),
+                      Statistics(),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 60,
+                child: Container(
+                  color: Colors.transparent,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
@@ -132,7 +147,7 @@ class Statistics extends StatelessWidget {
           color: Color.fromARGB(255, 0, 106, 74),
         ),
         child: ExpansionTile(
-          key: PageStorageKey<String>('key1'),
+          // key: PageStorageKey<String>('key1'),
           title: titleData('Статистика матча'),
           children: [
             Container(height: 1, color: Color.fromARGB(255, 0, 106, 74)),
@@ -167,7 +182,7 @@ Widget titleData(String text) {
       children: [
         Text(
           text,
-          style: TextStyle(color: Colors.white),
+          style: TextStyle(color: Colors.white, fontSize: 14),
         ),
       ],
     ),
@@ -199,3 +214,29 @@ Widget itemData(String text, [bool last = false]) {
     ),
   );
 }
+
+// // Статистика
+// Expanded(
+//   flex: 231,
+//   child: Container(
+//     color: Colors.red,
+//     // constraints:
+//     //     BoxConstraints(minHeight: 231, maxHeight: 231),
+//     // height: 350,
+//     child: Column(
+//       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//       children: [
+//         Column(
+//           children: [
+//             Text('МАТЧ СОСТОЯЛСЯ',
+//                 style: TextStyle(fontSize: 21)),
+//             Text('22.11.2020',
+//                 style: TextStyle(fontSize: 21)),
+//           ],
+//         ),
+//         Text('9:9', style: TextStyle(fontSize: 60)),
+//         Statistics(),
+//       ],
+//     ),
+//   ),
+// ),
